567b4d5c39583f98086c3d267f600c0c67afc768
syt-nju <nju221300094@163.com>
2024-09-09 21:50:29 +0800

    feat:添加AdaGrad

--- optim.py
+++ optim.py
@@ -32,6 +32,31 @@ class SGD(BaseOptimizer):
     def step(self):
         for param in self.parameters:
             param.data -= self.lr*param.grad
+class AdaGrad(BaseOptimizer):
+    '''
+        自适应学习率的梯度下降，在参数更新不均匀的情况下，可以比较好地自适应调整学习率，一般啥参数不用调
+        params: _params_t,
+        lr: float = ...,
+        lr_decay: float = ...(学习率衰减系数,一般对于AdaGrad，这个参数没什么用，默认值就行),
+        weight_decay: float = ...(L2正则化项系数),
+        initial_accumulator_value: float = ...,
+        eps: float = ...
+    '''
+    def __init__(self,parameters,lr=0.01,eps=1e-8,lr_decay=0.0,weight_decay=0.0,initial_accumulator_value=0.0):
+        super().__init__(parameters,lr)
+        self.eps = eps
+        self.G = [np.full(param.data.shape,initial_accumulator_value) for param in parameters]
+        self.weight_decay = weight_decay
+        self.lr_decay = lr_decay
+        self.lr = lr
+    def step(self):
+        self.G = [self.G[i]+param.grad**2 for i,param in enumerate(self.parameters)]
+        for i  in range(len(self.parameters)):
+            param = self.parameters[i]
+            param.data -= self.lr/(np.sqrt(self.G[i])+self.eps)*param.grad
+        
+        self.lr *= 1/(1+self.lr_decay)
+        
         
 if __name__=='__main__':
     #测试BGD
@@ -39,13 +64,16 @@ if __name__=='__main__':
     x=np.array(range(10)).reshape(-1,1)
     y_true=3*x+2+np.random.randn(10,1)*0.001
     layer=my_nn.MyLinearLayer(1,1,initial_policy='zeros')
-    optimizer=BGD([layer.weight,layer.bias],lr=0.01)
-    for i in range(1000):
+    optimizer=AdaGrad([layer.weight,layer.bias],lr=10)
+    loss_func=MSELoss()
+    for i in range(1000000):
         y_pred=layer.forward(MyTensor.MyTensor(x))
-        
-        loss=MSELoss().forward(y_pred,MyTensor.MyTensor(y_true))
+        loss=loss_func.forward(y_pred,MyTensor.MyTensor(y_true))
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
+        if loss.data<1e-5:
+            print("epcho",i)
+            break
     print(layer.weight.data,layer.bias.data)
     
--- temp.py
+++ temp.py
@@ -1,11 +1,22 @@
-import MyTensor
-import my_nn
 import numpy as np
-from torch.optim import SGD
-#构建一个y=ax+b的线性模型
-model=my_nn.MyLinearLayer(2,1,initial_policy='zeros')
-#创建一个形状为2，2的全1张量
-x=MyTensor.MyTensor(np.ones((2,2)),requires_grad=False)
-y=model.forward(x)
-y.backward()
-print(model.weight.grad)
+from torch import nn,tensor,float32
+from torch.optim import Adagrad
+from torch.nn import MSELoss
+#构造y=3x+2
+x=np.array(range(10)).reshape(-1,1)
+x=tensor(x,dtype=float32)
+y_true=3*x+2+np.random.randn(10,1)*0.001
+y_true=tensor(y_true,dtype=float32)
+model=nn.Linear(1,1)
+optimizer=Adagrad(model.parameters())
+loss_func=MSELoss()
+for i in range(100000):
+    optimizer.zero_grad()
+    y_pred=model(x)
+    loss=loss_func(y_pred,y_true)
+    loss.backward()
+    optimizer.step()
+    if loss.item()<1:
+        print("epcho",i)
+        break
+print("weight",model.weight.data,"bias",model.bias.data)
